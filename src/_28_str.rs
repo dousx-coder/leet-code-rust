8d327a8dd611e77e2017044778913996dd62e6fb
DouShaoxun <dousx_cn@163.com>
2024-11-30 16:26:53 +0800

    28 KMP

--- src/_28_str.rs
+++ src/_28_str.rs
@@ -1,3 +1,7 @@
+///
+/// https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+///
+/// KMP算法实现
 struct Solution {}
 
 impl Solution {
@@ -21,6 +25,8 @@ impl Solution {
                 }
             }
         }
+        // 将prefix_table整体向右移动一位
+        // [0,1,0,2,0]===> [-1,0,1,0,2]
         let mut index = n - 1;
         while index > 0 {
             prefix_table[index] = prefix_table[index - 1];
@@ -46,11 +52,14 @@ impl Solution {
                 i += 1;
                 j += 1;
             } else {
-                if prefix_table[j] < 0 {
+                // 字符不匹配
+                j = if prefix_table[j] < 0 {
+                    // j对应的前缀长度小于0，则需要重新从头匹配
                     i += 1;
-                    j = 0;
+                    0
                 } else {
-                    j = prefix_table[j] as usize;
+                    // 利用最长的公共(相同)前缀
+                    prefix_table[j] as usize
                 }
             }
         }
