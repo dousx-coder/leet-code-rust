5bf20751cde46bd94e11b0b2570652da7ab67324
DouShaoxun <dousx_cn@163.com>
2024-12-18 17:43:43 +0800

    二叉树测试数据图示

--- src/common/binary_tree.rs
+++ src/common/binary_tree.rs
@@ -32,6 +32,11 @@ impl TreeNode {
     ///
     /// # 示例
     /// ```
+    ///  //   3
+    ///  //  / \
+    ///  // 9  20
+    ///  //    / \
+    ///  //   15  7
     ///  use leet_code_rust::common::binary_tree::TreeNode;
     ///  let preorder = vec![3, 9, 20, 15, 7];
     ///  let inorder = vec![9, 3, 15, 20, 7];
@@ -195,11 +200,6 @@ mod tests {
 
     #[test]
     fn t1() {
-        //   3
-        //  / \
-        // 9  20
-        //    / \
-        //   15  7
         let preorder = vec![3, 9, 20, 15, 7];
         let inorder = vec![9, 3, 15, 20, 7];
         let root = TreeNode::build_binary_tree(&preorder, &inorder);
@@ -210,11 +210,6 @@ mod tests {
 
     #[test]
     fn t2() {
-        //   3
-        //  / \
-        // 9  20
-        //    / \
-        //   15  7
         let preorder = vec![3, 9, 20, 15, 7];
         let inorder = vec![9, 3, 15, 20, 7];
         let root = TreeNode::build_binary_tree(&preorder, &inorder);
