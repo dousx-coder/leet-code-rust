3a090e525ba30a1a95276fd5dc5a62a083b7f304
DouShaoxun <dousx_cn@163.com>
2024-12-28 17:44:44 +0800

    965 单值二叉树

---
+++ src/_965_is_unival_tree.rs
@@ -0,0 +1,64 @@
+use crate::common::binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+///
+/// 965 单值二叉树
+///
+/// https://leetcode.cn/problems/univalued-binary-tree/description/
+///
+struct Solution;
+impl Solution {
+    pub fn is_unival_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        let mut level_traversal = vec![];
+        // 等级
+        // 下一层
+        let mut next_level = VecDeque::new();
+        // 当前层
+        let mut curr_level = VecDeque::new();
+        curr_level.push_back(root.clone());
+        loop {
+            match curr_level.pop_front() {
+                Some(node) => match node {
+                    Some(rc) => {
+                        let v = rc.borrow().val;
+                        if !level_traversal.is_empty() && v != level_traversal[0] {
+                            return false;
+                        }
+                        level_traversal.push(v);
+                        next_level.push_back(rc.borrow().left.clone());
+                        next_level.push_back(rc.borrow().right.clone());
+                    }
+                    None => {}
+                },
+                None => {}
+            }
+            if curr_level.is_empty() && !next_level.is_empty() {
+                while let Some(rc) = next_level.pop_front() {
+                    curr_level.push_back(rc);
+                }
+                continue;
+            }
+            if curr_level.is_empty() && next_level.is_empty() {
+                break;
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn t1() {
+        let root = TreeNode::build_tree_by_sequential_storage(&vec![-1, 1, 2, 3, 4, 5], false);
+        assert_eq!(Solution::is_unival_tree(root), false);
+    }
+
+    #[test]
+    fn t2() {
+        let root = TreeNode::build_tree_by_sequential_storage(&vec![-1, 1, 1, 1, 1, 1], false);
+        assert_eq!(Solution::is_unival_tree(root), true);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -77,3 +77,4 @@ mod lcr_21_remove_nth_from_end;
 mod _111_min_depth;
 mod _47_permute_unique;
 mod _222_count_nodes;
+mod _965_is_unival_tree;
