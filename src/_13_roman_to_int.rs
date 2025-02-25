96d164403764b6d1e542b12dd5f0dd8e943265ad
DouShaoxun <dousx_cn@163.com>
2025-02-25 10:12:26 +0800

    13 通一灵码优化

--- src/_13_roman_to_int.rs
+++ src/_13_roman_to_int.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 struct Solution;
 ///
 /// https://leetcode.cn/problems/roman-to-integer/description/
@@ -5,14 +7,25 @@ impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
         let mut sum = 0;
         let mut prev = 0;
-        let str_list = s
-            .chars()
-            .into_iter()
-            .map(|c| c)
-            .rev()
-            .collect::<Vec<char>>();
-        for x in str_list {
-            let curr = Self::char_to_i32(x);
+
+        // 使用 HashMap 存储字符到整数的映射
+        let roman_map: HashMap<char, i32> = vec![
+            ('I', 1),
+            ('V', 5),
+            ('X', 10),
+            ('L', 50),
+            ('C', 100),
+            ('D', 500),
+            ('M', 1000),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        // 从字符串末尾向前遍历
+        for ch in s.chars().rev() {
+            let x = roman_map.get(&ch).unwrap_or(&0);
+            let curr = *x;
             if curr < prev {
                 sum -= curr;
             } else {
@@ -20,19 +33,8 @@ impl Solution {
             }
             prev = curr;
         }
+
         sum
-    }
-    fn char_to_i32(ch: char) -> i32 {
-        match ch {
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-            _ => 0,
-        }
     }
 }
 #[cfg(test)]
