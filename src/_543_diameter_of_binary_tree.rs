82dca0864838df542265976df947de59410f2edd
dousx <dousx_cn@163.com>
2025-02-26 21:56:59 +0800

    543

--- src/_543_diameter_of_binary_tree.rs
+++ src/_543_diameter_of_binary_tree.rs
@@ -1,15 +1,14 @@
 struct Solution;
 use crate::common::binary_tree::TreeNode;
 use std::cell::RefCell;
-use std::cmp::max;
 use std::rc::Rc;
 /// 543 二叉树的直径
 ///
 /// https://leetcode.cn/problems/diameter-of-binary-tree/
 impl Solution {
     pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut max = i32::MIN;
-        Self::dfs(root, &mut max);
+        let mut max = 0;
+        let i = Self::dfs(root, &mut max);
         max
     }
     fn dfs(root: Option<Rc<RefCell<TreeNode>>>, curr_max: &mut i32) -> i32 {
@@ -22,15 +21,27 @@ impl Solution {
                 if right.is_none() && left.is_none() {
                     return 0;
                 }
-                let right = Self::dfs(right, curr_max);
-                let left = Self::dfs(left, curr_max);
-                //let child_max = max(left + 1, right + 1);
-                //let curr_sum = left + 1 + right;
-                //if curr_sum > *curr_max {
-                //    *curr_max = curr_sum;
-                //}
-                //max(curr_sum, 1)
-                panic!()
+                let val = mbr.val;
+                let mut curr_sum = 0;
+                let mut child_max = 0;
+                if left.is_some() {
+                    let left = Self::dfs(left, curr_max);
+                    curr_sum += left + 1;
+                    if left > child_max || child_max == 0 {
+                        child_max = left + 1;
+                    }
+                }
+                if right.is_some() {
+                    let right = Self::dfs(right, curr_max);
+                    curr_sum += right + 1;
+                    if right >= child_max || child_max == 0 {
+                        child_max = right + 1;
+                    }
+                }
+                if curr_sum > *curr_max {
+                    *curr_max = curr_sum;
+                }
+                child_max
             }
         }
     }
@@ -60,5 +71,28 @@ mod tests {
         let root = TreeNode::build_binary_tree(&preorder, &inorder);
         let ans = Solution::diameter_of_binary_tree(root);
         assert_eq!(ans, 1);
+    }
+
+    #[test]
+    fn t3() {
+        let preorder = vec![1];
+        let inorder = vec![1];
+        let root = TreeNode::build_binary_tree(&preorder, &inorder);
+        let ans = Solution::diameter_of_binary_tree(root);
+        assert_eq!(ans, 0);
+    }
+
+    #[test]
+    fn t4() {
+        //     2 
+        //    /
+        //   3
+        //  /
+        // 1
+        let preorder = vec![2, 3, 1];
+        let inorder = vec![1, 3, 2];
+        let root = TreeNode::build_binary_tree(&preorder, &inorder);
+        let ans = Solution::diameter_of_binary_tree(root);
+        assert_eq!(ans, 2);
     }
 }
