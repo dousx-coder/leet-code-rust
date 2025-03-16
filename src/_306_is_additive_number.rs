38ae87541cc1d4bbfcb27b064875816f63fcbf3a
DouShaoxun <dousx_cn@163.com>
2025-03-16 14:12:51 +0800

    feat(306): 优化大数相加算法
    - 修改 backtracking 函数中的 parse 数组，存储数字的起始和结束索引
    - 实现 eq函数，用于比较两个数字之和与第三个数字的大小关系
    - 优化算法以处理大数相加，避免使用 u128 类型

--- src/_306_is_additive_number.rs
+++ src/_306_is_additive_number.rs
@@ -7,7 +7,7 @@ impl Solution {
         let mut num = num.chars().collect::<Vec<char>>();
         Self::backtracking(0, &num, &mut vec![])
     }
-    fn backtracking(start: usize, num: &Vec<char>, parse: &mut Vec<u128>) -> bool {
+    fn backtracking(start: usize, num: &Vec<char>, parse: &mut Vec<(usize, usize)>) -> bool {
         let len = num.len();
         if start >= len {
             return false;
@@ -27,8 +27,8 @@ impl Solution {
                 i
             };
             for end in i..=j {
-                let c = Self::parse_num(start, num, end);
-                parse.push(c);
+                let c = (start, end);
+                parse.push((start, end));
                 if parse.len() < 3 {
                     if Self::backtracking(end + 1, num, parse) {
                         return true;
@@ -37,20 +37,17 @@ impl Solution {
                     }
                     continue;
                 }
-                let a = parse[parse.len() - 3];
-                let b = parse[parse.len() - 2];
-                let c = parse[parse.len() - 1];
-
-                if a + b < c {
+                let check = Self::eq(num, parse);
+                if check < 0 {
                     // 剪枝
                     parse.pop();
                     break;
                 }
-                if a + b > c {
+                if check > 0 {
                     parse.pop();
                     continue;
                 }
-                if a + b == c {
+                if check == 0 {
                     if i == len - 1 {
                         return true;
                     };
@@ -64,13 +61,63 @@ impl Solution {
         }
         false
     }
+    /// a+b和c大小判断 (u128长度适用于大部分测试用例,自定义比较适用于任意长度的测试用例)
+    ///
+    /// -1: a + b < c
+    ///
+    /// 0 : a + b = c
+    ///
+    /// 1 : a + b > c (剪枝)
+    fn eq(num: &Vec<char>, parse: &mut Vec<(usize, usize)>) -> i8 {
+        let parse_len = parse.len();
+        let a = parse[parse_len - 3];
+        let a = &num[a.0..=a.1];
 
-    fn parse_num(start: usize, num: &Vec<char>, i: usize) -> u128 {
-        num[start..=i]
-            .iter()
-            .collect::<String>()
-            .parse::<u128>()
-            .unwrap()
+        let b = parse[parse_len - 2];
+        let b = &num[b.0..=b.1];
+
+        let c = parse[parse_len - 1];
+        let c = &num[c.0..=c.1];
+        // 进位
+        let mut carry = 0;
+        let mut sum = vec![];
+        let mut ia = (a.len() - 1) as i32;
+        let mut ib = (b.len() - 1) as i32;
+        while ia >= 0 || ib >= 0 {
+            let add1 = if ia < 0 {
+                0
+            } else {
+                a[ia as usize].to_digit(10).unwrap() as u8
+            };
+            let add2 = if ib < 0 {
+                0
+            } else {
+                b[ib as usize].to_digit(10).unwrap() as u8
+            };
+            ia -= 1;
+            ib -= 1;
+            let added = add1 + add2 + carry;
+            sum.push(added % 10);
+            carry = added / 10;
+        }
+        if carry != 0 {
+            sum.push(carry);
+        }
+        let len = sum.len();
+        let c_len = c.len();
+        if len < c_len {
+            return -1;
+        }
+        if len > c_len {
+            return 1;
+        }
+        for i in (0..=len - 1).rev() {
+            let x = c[len - 1 - i].to_digit(10).unwrap() as u8;
+            if sum[i] != x {
+                return if sum[i] > x { 1 } else { -1 };
+            }
+        }
+        0
     }
 }
 #[cfg(test)]
