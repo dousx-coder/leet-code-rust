d47b28f3623efc34250d67153a399f482ad64e7c
dousx <dousx_cn@163.com>
2025-03-26 22:03:26 +0800

    feat(_491): 优化 Find Subsequences 题目解决方案
    - 添加 backtracking1 方法，使用 HashSet 来标记当前层已用过的元素，避免重复
    - 添加 backtracking2 方法，使用固定长度的布尔数组来去重，进一步优化性能
    - 增加测试用例验证新方法的正确性

--- src/_491_find_subsequences.rs
+++ src/_491_find_subsequences.rs
@@ -9,29 +9,59 @@ impl Solution {
         let mut ans = Vec::new();
         let used = &mut vec![false; nums.len()];
         let path = &mut vec![];
-        Self::backtracking(0, &nums, path, &mut ans);
+        Self::backtracking1(0, &nums, path, &mut ans);
         ans
     }
-    fn backtracking(start: usize, nums: &Vec<i32>, path: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
+    /// set标记当前层已用过的元素，避免重复
+    ///
+    /// 耗时：0ms，内存：4MB
+    fn backtracking1(start: usize, nums: &Vec<i32>, path: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
         if start >= nums.len() {
             return;
         }
         // 同一个父节点下，当前层不能出现相同的元素
         let mut used = HashSet::new();
         for i in start..nums.len() {
-            if !path.is_empty() && nums[i] < *path.last().unwrap() {
-                continue;
-            }
             if used.contains(&nums[i]) {
                 // 使用set标记当前层已用过的元素
                 continue;
             }
+            if !path.is_empty() && nums[i] < *path.last().unwrap() {
+                continue;
+            }
+
             used.insert(nums[i]);
             path.push(nums[i]);
             if path.len() >= 2 {
                 ans.push(path.clone());
             }
-            Self::backtracking(i + 1, nums, path, ans);
+            Self::backtracking1(i + 1, nums, path, ans);
+            path.pop();
+        }
+    }
+    /// 数组去重，根据数值范围创建数组长度为201的bool数组，避免使用set
+    /// 耗时：1ms，内存：4MB
+    fn backtracking2(start: usize, nums: &Vec<i32>, path: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
+        if start >= nums.len() {
+            return;
+        }
+        // 数值范围[-100, 100]
+        // 同一个父节点下，当前层不能出现相同的元素
+        let mut used = vec![false; 201];
+        for i in start..nums.len() {
+            let j = (nums[i] + 100) as usize;
+            if used[j] {
+                continue;
+            }
+            if !path.is_empty() && nums[i] < *path.last().unwrap() {
+                continue;
+            }
+            used[j] = true;
+            path.push(nums[i]);
+            if path.len() >= 2 {
+                ans.push(path.clone());
+            }
+            Self::backtracking2(i + 1, nums, path, ans);
             path.pop();
         }
     }
@@ -39,6 +69,7 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use maplit::hashset;
     use std::collections::HashSet;
@@ -55,6 +86,19 @@ mod tests {
     fn t2() {
         let expected = hashset! {vec![4,4]};
         let ans = Solution::find_subsequences(vec![4, 4, 3, 2, 1]);
+        assert_eq!(expected.len(), ans.len());
+        let ans = HashSet::from_iter(ans.into_iter());
+        assert_eq!(expected, ans);
+    }
+
+    #[test]
+    fn t3() {
+        let expected = hashset! {vec![4,6],vec![4,6,7],vec![4,6,7,7],vec![4,7],vec![4,7,7],vec![6,7],vec![6,7,7],vec![7,7]};
+        let nums = vec![4, 6, 7, 7];
+        let mut ans = Vec::new();
+        let used = &mut vec![false; nums.len()];
+        let path = &mut vec![];
+        Solution::backtracking2(0, &nums, path, &mut ans);
         assert_eq!(expected.len(), ans.len());
         let ans = HashSet::from_iter(ans.into_iter());
         assert_eq!(expected, ans);
