5436b8c5442c5e2d3e8c01ccb2394d66fce34bb1
dousx <dousx_cn@163.com>
2025-03-26 21:52:18 +0800

    refactor(_491): 优化递归函数参数设计
    - 移除了不必要的 HashSet，改用 Vec 存储结果，减少内存消耗
    - 优化了去重逻辑，使用固定大小的数组替代动态分配的 Vec<bool>
    - 简化了递归函数参数，提高了代码可读性和性能

--- src/_491_find_subsequences.rs
+++ src/_491_find_subsequences.rs
@@ -7,36 +7,34 @@ struct Solution;
 impl Solution {
     pub fn find_subsequences(nums: Vec<i32>) -> Vec<Vec<i32>> {
         // nums是无序的，只能用hashSet+used标记双重去重(used剪枝)
-        let mut ans = HashSet::new();
+        let mut ans = Vec::new();
         let used = &mut vec![false; nums.len()];
         let path = &mut vec![];
-        Self::backtracking(0, &nums, used, path, &mut ans);
-        ans.into_iter().collect()
+        Self::backtracking(0, &nums, path, &mut ans);
+        ans
     }
-    fn backtracking(
-        start: usize,
-        nums: &Vec<i32>,
-        used: &mut Vec<bool>,
-        path: &mut Vec<i32>,
-        ans: &mut HashSet<Vec<i32>>,
-    ) {
+    fn backtracking(start: usize, nums: &Vec<i32>, path: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>) {
         if start >= nums.len() {
             return;
         }
+        // 数值范围[-100, 100]
+        // 同一个父节点下，当前层不能出现相同的元素
+        let mut used = vec![false; 201];
         for i in start..nums.len() {
-            if used[i] {
+            let j = (nums[i] + 100) as usize;
+            if used[j] {
                 continue;
             }
-            if path.is_empty() || nums[i] >= *path.last().unwrap() {
-                used[i] = true;
-                path.push(nums[i]);
-                if path.len() >= 2 {
-                    ans.insert(path.clone());
-                }
-                Self::backtracking(i + 1, nums, used, path, ans);
-                path.pop();
-                used[i] = false;
+            if !path.is_empty() && nums[i] < *path.last().unwrap() {
+                continue;
             }
+            used[j] = true;
+            path.push(nums[i]);
+            if path.len() >= 2 {
+                ans.push(path.clone());
+            }
+            Self::backtracking(i + 1, nums, path, ans);
+            path.pop();
         }
     }
 }
