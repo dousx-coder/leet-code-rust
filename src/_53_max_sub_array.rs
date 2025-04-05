778dc41df77aaab0485e6ece08316dd0b3a006c7
DouShaoxun <dousx_cn@163.com>
2025-04-05 18:40:58 +0800

    refactor(_53_max_sub_array): 优化最大子数组和算法
    - 使用 iter().skip(1) 替代 for 循环，提高代码可读性
    - 用 max 方法替代了手动比较，简化了逻辑

--- src/_53_max_sub_array.rs
+++ src/_53_max_sub_array.rs
@@ -1,28 +1,18 @@
-///
-/// 53 最大子数组和
-///
-/// https://leetcode.cn/problems/maximum-subarray/
 struct Solution;
+
 impl Solution {
     /// 找出一个具有最大和的连续子数组
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        // 当前连续子数组的和
         let mut curr = nums[0];
-        // 最大连续子数组的和
         let mut max = nums[0];
-        for i in 1..nums.len() {
-            if curr < 0 {
-                curr = nums[i];
-            } else {
-                curr += nums[i]
-            }
-            if curr > max {
-                max = curr;
-            }
+        for &num in nums.iter().skip(1) {
+            curr = num.max(curr + num);
+            max = max.max(curr);
         }
         max
     }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,6 +29,7 @@ mod tests {
     fn t2() {
         assert_eq!(Solution::max_sub_array(vec![1]), 1);
     }
+
     #[test]
     fn t3() {
         assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
