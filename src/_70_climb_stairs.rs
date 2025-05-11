0d91b19db03f5cdd75d60db12a2c0b7c83f0749e
DouShaoxun <dousx_cn@163.com>
2025-05-11 16:44:07 +0800

    feat: 70 爬楼梯(dp compress)

--- src/_70_climb_stairs.rs
+++ src/_70_climb_stairs.rs
@@ -4,6 +4,9 @@
 struct Solution;
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
+        Self::dp(n)
+    }
+    pub fn dp(n: i32) -> i32 {
         if n == 1 {
             return 1;
         }
@@ -12,7 +15,7 @@ impl Solution {
         }
         let n = n as usize;
         let mut dp = vec![0; n + 1];
-        // 初始化数组，上1阶有1种方法，上2阶有2种方法,
+        // 初始化数组，上1阶有1种方法，上2阶有2种方法
         dp[1] = 1;
         dp[2] = 2;
         for i in 3..=n {
@@ -20,6 +23,24 @@ impl Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         dp[n]
+    }
+    fn climb_stairs_dp_compress(n: i32) -> i32 {
+        if n == 1 {
+            return 1;
+        }
+        if n == 2 {
+            return 2;
+        }
+        // 状态压缩
+        let mut a = 1;
+        let mut b = 2;
+        let mut c = 0;
+        for i in 3..=n {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        c
     }
 }
 
@@ -34,5 +55,14 @@ mod tests {
     #[test]
     fn t2() {
         assert_eq!(Solution::climb_stairs(2), 2);
+    }
+
+    #[test]
+    fn t3() {
+        assert_eq!(Solution::climb_stairs_dp_compress(3), 3);
+    }
+    #[test]
+    fn t4() {
+        assert_eq!(Solution::climb_stairs_dp_compress(2), 2);
     }
 }
