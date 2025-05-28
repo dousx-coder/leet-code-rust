e20a7f96f0cd1307da221d68e8fe5a034d0287c0
dousx <dousx_cn@163.com>
2025-05-28 22:34:29 +0800

    feat(_494_find_target_sum_ways): 动态规划解法

--- src/_494_find_target_sum_ways.rs
+++ src/_494_find_target_sum_ways.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 /// 494 目标和
 ///
 /// https://leetcode.cn/problems/target-sum/?envType=problem-list-v2&envId=backtracking
@@ -14,6 +16,36 @@ impl Solution {
         ans += Self::backtracking(pos + 1, nums, target - nums[pos]);
         ans += Self::backtracking(pos + 1, nums, target + nums[pos]);
         ans
+    }
+
+    ///
+    /// 动态规划解法
+    ///
+    /// 假设数字前面添加+号的数字总和为left，添加-号的总和为right，则可得到
+    ///
+    /// 等式1:left+right=sum(nums)
+    ///
+    /// 等式2:left-right=target
+    ///
+    /// 由等式1和等式2可得：2left=sum(nums)+target
+    ///
+    /// left=(sum(nums)+target)/2,即转换为背包问题，
+    /// 求nums中任取多少个数字达到left共有多少种方式
+    fn dp(nums: Vec<i32>, target: i32) -> i32 {
+        let sum: i32 = nums.iter().sum();
+        // 用sum+target，leetcode会过不去?
+        if sum < target || (sum - target) % 2 != 0 {
+            return 0;
+        }
+        let right = ((sum - target) / 2) as usize;
+        let mut dp = vec![0; right + 1];
+        dp[0] = 1;
+        for x in nums {
+            for j in (x as usize..=right).rev() {
+                dp[j] += dp[j - x as usize];
+            }
+        }
+        dp[right]
     }
 }
 
@@ -45,5 +77,24 @@ mod tests {
             1,
         );
         assert_eq!(524288, ans);
+    }
+
+    #[test]
+    fn t5() {
+        let ans = Solution::dp(
+            vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            1,
+        );
+        assert_eq!(524288, ans);
+    }
+    #[test]
+    fn t6() {
+        let ans = Solution::dp(vec![100], -200);
+        assert_eq!(0, ans);
+    }
+    #[test]
+    fn t7() {
+        let ans = Solution::dp(vec![1000], -1000);
+        assert_eq!(1, ans);
     }
 }
