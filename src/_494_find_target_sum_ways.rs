9340b557681c9e5131a5e8ac5b9c317c200a33cf
DouShaoxun <dousx_cn@163.com>
2025-05-29 11:14:32 +0800

    refactor(_494):解释 dp 数组的定义和计算过程

--- src/_494_find_target_sum_ways.rs
+++ src/_494_find_target_sum_ways.rs
@@ -33,20 +33,36 @@ impl Solution {
     /// 求nums中任取多少个数字达到left共有多少种方式
     fn dp(nums: Vec<i32>, target: i32) -> i32 {
         let sum: i32 = nums.iter().sum();
-        // nums=[1000],target=-1000,这种情况是把唯一的数字变成负数即可，有1种方式
-        // 如果用加法，得到的数字和为sum+target，即left=0,然nums中只有一个1000,则会得到错误的解
-        // 如果是减法，则得到right=(1000-(-1000))/2=1000,可以得到一种解法
+        // 针对特殊情况的分析：如 nums=[1000], target=-1000
+        // 此时只能将唯一数字取负，有一种合法方式。
+        // 若通过 left=(sum + target)/2 计算，则会得到left=0，但nums中没有0导致错误结果
         if sum < target || (sum - target) % 2 != 0 {
             return 0;
         }
         let right = ((sum - target) / 2) as usize;
         let mut dp = vec![0; right + 1];
         dp[0] = 1;
+
+        // dp[j] 表示从前缀数组中选出若干个元素组成和为j的方式数量
+        // 例如：dp[5]表示 数组中取任意数字，共有多少种组合方式使得总和为5
+        //
+        // 如果数组中存在数字1，则dp[5]=dp[4]，如果存在数组1，则组成dp[4]的这些组合中再加上1，就能得到组合和为5
+        // 所以dp[right]应该是下面所有情况总和
+        //
+        // 如果存在right，则加上dp[0]
+        // 如果存在right-1，则加上dp[1]
+        // 如果存在right-2，则加上dp[2]
+        // ...
+
         for x in nums {
-            for j in (x as usize..=right).rev() {
-                dp[j] += dp[j - x as usize];
+            let val = x as usize;
+            for j in (val..=right).rev() {
+                // 如果此时val是7
+                // dp[10]=dp[10]+dp[3]
+                dp[j] += dp[j - val];
             }
         }
+
         dp[right]
     }
 }
