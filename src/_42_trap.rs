0823d1e68cf40fdafdb438386843f8fafd85a878
DouShaoxun <dousx_cn@163.com>
2025-06-05 14:10:47 +0800

    test(trap): (42)优化代码注释

--- src/_42_trap.rs
+++ src/_42_trap.rs
@@ -30,7 +30,12 @@ impl Solution {
 
         let mut stack = Vec::with_capacity(len);
         let mut result = 0;
-
+        // 纵坐标单位是10
+        // (40)-        
+        // (30)-       -
+        // (20)- - -   -
+        // (10)- - - - -
+        //     0 1 2 3 4
         for i in 0..len {
             let curr_height = height[i];
             while !stack.is_empty() && height[*stack.last().unwrap()] < curr_height {
@@ -38,10 +43,15 @@ impl Solution {
                 if stack.is_empty() {
                     break;
                 }
+                // 当i为4时,curr_height=30
+                // 第1次进入循环(top=3,left=2,height=10),此时栈内栈内元素为[0,1,2]
+                // 第2次进入循环(top=2,left=1,height=0),此时栈内栈内元素为[0,1]
+                // 第3次进入循环(top=1,left=0,height=10),此时栈为空，结束当前循环，将下标4压入栈，下次弹出后发现只有1个元素结束外层循环
                 let left = *stack.last().unwrap();
                 let min_height = curr_height.min(height[left]);
-                let distance = (i - left - 1) as i32;
-                result += distance * (min_height - height[top]);
+                let distance = (i - (left + 1)) as i32;
+                let height = min_height - height[top];
+                result += distance * height;
             }
 
             stack.push(i);
@@ -69,5 +79,10 @@ mod tests {
     #[test]
     fn t3() {
         assert_eq!(Solution::trap_monotonic_stack(vec![4, 2, 0, 3, 2, 5]), 9);
+    }
+
+    #[test]
+    fn t4() {
+        assert_eq!(Solution::trap_monotonic_stack(vec![40, 20, 20, 10, 30]), 40);
     }
 }
