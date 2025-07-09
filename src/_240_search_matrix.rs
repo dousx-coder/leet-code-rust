2233ee83ab8c679d853a10bba36f0a68d911dc7f
DouShaoxun <dousx_cn@163.com>
2025-07-09 16:40:47 +0800

    feat(_240_search_matrix): 二分查找

--- src/_240_search_matrix.rs
+++ src/_240_search_matrix.rs
@@ -3,32 +3,51 @@
 /// https://leetcode.cn/problems/search-a-2d-matrix-ii/?envType=problem-list-v2&envId=binary-search
 struct Solution;
 impl Solution {
+    fn search(
+        matrix: &Vec<Vec<i32>>,
+        visited: &mut Vec<Vec<bool>>,
+        target: i32,
+        row: usize,
+        col: usize,
+        m: usize,
+        n: usize,
+    ) -> bool {
+        if row >= m || col >= n || visited[row][col] {
+            return false;
+        }
+        if matrix[row][col] == target {
+            return true;
+        }
+        // 标记已经访问
+        visited[row][col] = true;
+        if matrix[row][col] > target {
+            // 向左上方查找
+            (match row.checked_sub(1) {
+                Some(r) => Self::search(matrix, visited, target, r, col, m, n),
+                None => false,
+            }) || (match col.checked_sub(1) {
+                None => false,
+                Some(c) => Self::search(matrix, visited, target, row, c, m, n),
+            })
+
+        } else {
+            // 向右下方查找
+            Self::search(matrix, visited, target, row + 1, col, m, n)
+                || Self::search(matrix, visited, target, row, col + 1, m, n)
+        }
+    }
+
     ///
     /// 每行的元素从左到右升序排列。
     ///
     /// 每列的元素从上到下升序排列。
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        //  m行，n列
         let m = matrix.len();
         let n = matrix[0].len();
-        let mut row = 0;
-        let mut col = n - 1;
-        loop {
-            if matrix[row][col] == target {
-                return true;
-            }
-            if matrix[row][col] < target {
-                row += 1;
-                if row >= m {
-                    return false;
-                }
-            } else {
-                if col < 1 {
-                    return false;
-                }
-                col -= 1;
-            }
-        }
+        let mut visited = vec![vec![false; n]; m];
+        let row = m / 2;
+        let col = n / 2;
+        Self::search(&matrix, &mut visited, target, row, col, m, n)
     }
 }
 
@@ -63,5 +82,18 @@ mod tests {
     #[test]
     fn t3() {
         assert!(Solution::search_matrix(vec![vec![-1, 3]], 3));
+    }
+    #[test]
+    fn t4() {
+        assert!(Solution::search_matrix(
+            vec![
+                vec![1, 3, 5, 7, 9],
+                vec![2, 4, 6, 8, 10],
+                vec![11, 13, 15, 17, 19],
+                vec![12, 14, 16, 18, 20],
+                vec![21, 22, 23, 24, 25]
+            ],
+            13
+        ));
     }
 }
